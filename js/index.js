f4ab4cba0eaca2354449964eb844afb670c7e0b1
SilentStorm2k <47686604+SilentStorm2k@users.noreply.github.com>
2024-01-30 01:04:08 +0530

    feat: added ability to put book object into html display with delete button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const display = document.querySelector('main');
 
 function Book(title, author, pages, isAlreadyRead) {
     this.title = title;
@@ -8,4 +9,77 @@ function Book(title, author, pages, isAlreadyRead) {
     this.info = () => `${this.title} by ${this.author}, ${this.pages} pages, ${this.isAlreadyRead ? "already read" : "not read yet"}`;
 };
 
+function addBookToLibrary (book) {
+    myLibrary.push(book);
+    showBooks();
+}
+
+function showBooks () {
+    while (display.firstChild) {
+        display.removeChild(display.firstChild);
+    }
+    for (let book of myLibrary)
+        addBookToDisplay(book);
+    addNewBookButton();
+}
+
+function addBookToDisplay (book) {
+    const title = document.createElement('div');
+    const author = document.createElement('div');
+    const pages = document.createElement('div');
+    const isAlreadyRead = document.createElement('div');
+    const read = document.createElement('input');
+    const notRead = document.createElement('input');
+    const titleText = document.createElement('div');
+    const authorText = document.createElement('div');
+    const pagesText = document.createElement('div');
+    const isAlreadyReadText = document.createElement('div');
+    const deleteButton = document.createElement('button');
+    const entry = document.createElement('div');
+    entry.classList.add('card');
+    title.textContent = book.title;
+    author.textContent = book.author;
+    pages.textContent = book.pages;
+    read.type = "radio";
+    read.checked = book.isAlreadyRead;
+    notRead.type = "radio"
+    notRead.checked = !book.isAlreadyRead;
+    read.name = book.info();
+    notRead.name = book.info();
+    isAlreadyRead.append(read, notRead);
+    titleText.textContent = "Title:";
+    authorText.textContent = "Author:";
+    pagesText.textContent = "Pages:";
+    deleteButton.textContent = "Delete";
+    deleteButton.addEventListener("click", () => {
+        const bookIndex = myLibrary.indexOf(book);
+        if (bookIndex > -1)
+            myLibrary.splice(bookIndex, 1);
+        console.log(myLibrary, bookIndex)
+        showBooks();
+    });
+    isAlreadyReadText.textContent = "Already read:";
+    entry.append(titleText, title, authorText, author, pagesText, pages, isAlreadyReadText, isAlreadyRead, deleteButton);
+    display.appendChild(entry);
+}
+
+function addNewBookButton () {
+    const entry = document.createElement('div');
+    entry.classList.add('card');
+    const addButton = document.createElement('button');
+    addButton.style.gridArea = "1 / 1 / 6 / 3";
+    addButton.textContent = "Add new book";
+    addButton.addEventListener("click", getNewBook);
+    entry.appendChild(addButton);
+    display.appendChild(entry);
+}
+
+function getNewBook () {
+    
+}
+
 const book1 = new Book("The hobbit", "J.R.R. Tolkien", 295, false);
+const book2 = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 223, false);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
